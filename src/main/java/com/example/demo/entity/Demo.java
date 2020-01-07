d8da0a4ade7361d6f02125692c800a6bd36bc59e
zj <zj6897@qq.com>
2020-01-07 13:39:43 +0800

    Demo

---
+++ src/main/java/com/example/demo/entity/Demo.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+/**
+ * @author zhangzhijie
+ * 2020/1/7 13:37
+ */
+public class Demo {
+
+    /**
+     * userName
+     */
+    private String userName;
+    /**
+     * password
+     */
+    private String passWord;
+}
